b87f8e756397e2a0dca540268f9ed777b900ce4f
Felipe <felipevalentim@live.com>
2019-11-24 22:15:44 -0300

    Test

--- JavaPOO/src/ContaCorrente/ContaTeste.java
+++ JavaPOO/src/ContaCorrente/ContaTeste.java
@@ -8,8 +8,17 @@ public class ContaTeste {
     @Test
     public void deveSacar(){
         Conta conta = new Conta("Felipe", 1,50);
-        conta.saldo();
-        
-
+        conta.sacar(30);
+        Assert.assertEquals(20,20);
+        System.out.println("Operação de teste: Saque realizada com sucesso");
+        System.out.println("Saldo atual: " + conta.saldo + " Reais");
+    }
+    @Test
+    public void deveDepositar(){
+        Conta conta = new Conta("Felipe", 1,50);
+        conta.depositar(30);
+        Assert.assertEquals(80,80);
+        System.out.println("Operação de teste: Depósito realizada com sucesso");
+        System.out.println("Saldo atual: " + conta.saldo + " Reais");
     }
 }
