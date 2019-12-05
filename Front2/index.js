02c3133cec15573cf8bcbd83d8cfd978f6b6493c
Felipe <alunos@intranet.lan>
2019-12-05 16:44:29 -0300

    Front2

--- Front2/index.js
+++ Front2/index.js
@@ -1,27 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
+    title: 'Aula de Front-End',
     message: 'Hello Vue!',
     aula: 'Front-End com Vue.JS',
-    states: []
+    states: [{"id":2,"nome":"Paraná"},{"id":3,"nome":"Rio de Janeiro"},{"id":4,"nome":"Bahia"}],
+    stateName: '',
 },
     methods: {
+        setStateName(event){
+            this.stateName = event.target.value;
+        },
+
+        addStateList(){
+                const id = this.states.length;
+                this.states.push({
+                    id: id,
+                    nome : this.stateName
+                });
+        },
+
         changeInputValue(event){
             console.log(event)
             this.message = event.target.value;
         },
+
         getStates(){
-            fetch('https://rest-banco-api.herokuapp.com/estados')
-            .then(content => content.json())
-            .then(message => {
-                
-            })
-            this.states = ['Paraná', 'São Paulo']
+            //this.states = ['Paraná', 'São Paulo']
         }
-    },
+        },
+
         created(){
             console.log('Criou o componente')
             this.getStates()
-
-    }
+        },
+        
 })
